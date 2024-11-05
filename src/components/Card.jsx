81b7eb697586ac6c9309790d37e22d7882db3f09
Vikus228 <vika.kolesnikovs.1990@gmail.com>
2024-11-05 17:34:53 +0300

    .

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,14 @@
 function Card(props) {
     return(
         <div className="cardHeroes">
-            <h4 className="cardTitle">{props.title}</h4>
-            <img src={props.src} alt={props.title} className="cardFhoto"/>
-            <p className="cardText">{props.text}</p>
+            <h4 className="cardTitle">{props.name}</h4>
+            <img src={props.url} alt={props.name} className="cardFhoto"/>
+            <p className="cardText">{props.universe}</p>
+            <p className="cardText">{props.alterego}</p>
+            <p className="cardText">{props.occupation}</p>
+            <p className="cardText">{props.friends}</p>
+            <p className="cardText">{props.superpowers}</p>
+            <p className="cardText">{props.info}</p>
         </div>
     );
 };
